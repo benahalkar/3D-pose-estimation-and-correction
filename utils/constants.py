2030d5dfd35c2aa8b8d26bdda10c19c9618ebc07
benahalkar <benahalkar@gmail.com>
2025-02-18 18:33:42 -0500

    beautifying utils

--- utils/constants.py
+++ utils/constants.py
@@ -1,167 +1,197 @@
 """
 EECS 6692: Deep Learning on the Edge
-File contains constants for use through the application
+File contains constants for use throughout the application.
+
+Source: https://developers.google.com/mediapipe/solutions/vision/pose_landmarker
 """
 
 from enum import Enum
 
-# source: https://developers.google.com/mediapipe/solutions/vision/pose_landmarker
 
 class Constants(Enum):
+    """
+    Enum class to hold constant values used throughout the application.
+    """
+
     BODY_KP = {
-    "nose": 0,
-    "left_shoulder": 11, 
-    "right_shoulder": 12,
-    "left_elbow": 13, 
-    "right_elbow": 14,
-    "left_wrist": 15, 
-    "right_wrist": 16,
-    "left_hip": 23, 
-    "right_hip": 24,
-    "left_knee": 25, 
-    "right_knee": 26,
-    "left_ankle": 27, 
-    "right_ankle": 28,
-    "left_heel": 29, 
-    "right_heel": 30,
-    "left_foot_index": 31, 
-    "right_foot_index": 32
+        "nose": 0,
+        "left_shoulder": 11,
+        "right_shoulder": 12,
+        "left_elbow": 13,
+        "right_elbow": 14,
+        "left_wrist": 15,
+        "right_wrist": 16,
+        "left_hip": 23,
+        "right_hip": 24,
+        "left_knee": 25,
+        "right_knee": 26,
+        "left_ankle": 27,
+        "right_ankle": 28,
+        "left_heel": 29,
+        "right_heel": 30,
+        "left_foot_index": 31,
+        "right_foot_index": 32,
     }
+    """
+    Dictionary mapping body keypoint names to their corresponding indices
+    as defined by MediaPipe Pose Landmarker.
+    """
 
     YOGA_CLASSES = {
-        1: "chair_pose", 
-        2: "downward_facing_dog", 
-        3: "revolved_triangle", 
-        4: "half_moon", 
-        5: "tree_pose"
+        1: "chair_pose",
+        2: "downward_facing_dog",
+        3: "revolved_triangle",
+        4: "half_moon",
+        5: "tree_pose",
     }
+    """
+    Dictionary mapping integer identifiers to yoga pose names.
+    """
 
     GROUND_TRUTHS = {
         "chair_pose": {
-            "armpit_left":180,
-            "armpit_right":180,
-            "elbow_left":180,
-            "elbow_right":180,
-            "hip_left":60,
-            "hip_right":60,
-            "knee_left":100,
-            "knee_right":100
+            "armpit_left": 180,
+            "armpit_right": 180,
+            "elbow_left": 180,
+            "elbow_right": 180,
+            "hip_left": 60,
+            "hip_right": 60,
+            "knee_left": 100,
+            "knee_right": 100,
         },
         "downward_facing_dog": {
-            "armpit_left":180,
-            "armpit_right":180,
-            "elbow_left":180,
-            "elbow_right":180,
-            "hip_left":90,
-            "hip_right":90,
-            "knee_left":180,
-            "knee_right":180,
-            "ankle_left":60,
-            "ankle_right":60
+            "armpit_left": 180,
+            "armpit_right": 180,
+            "elbow_left": 180,
+            "elbow_right": 180,
+            "hip_left": 90,
+            "hip_right": 90,
+            "knee_left": 180,
+            "knee_right": 180,
+            "ankle_left": 60,
+            "ankle_right": 60,
         },
         "revolved_triangle": {
-            "armpit_left":90,
-            "armpit_right":90,
-            "elbow_left":180,
-            "elbow_right":180,
-            "hip_left":60,
-            "hip_right":120,
-            "knee_left":180,
-            "knee_right":180,
-            "ankle_left":120,
-            "ankle_right":60
+            "armpit_left": 90,
+            "armpit_right": 90,
+            "elbow_left": 180,
+            "elbow_right": 180,
+            "hip_left": 60,
+            "hip_right": 120,
+            "knee_left": 180,
+            "knee_right": 180,
+            "ankle_left": 120,
+            "ankle_right": 60,
         },
         "half_moon": {
-            "armpit_left":90,
-            "armpit_right":90,
-            "elbow_left":180,
-            "elbow_right":180,
-            "hip_left":90,
-            "hip_right":180,
-            "knee_left":180,
-            "knee_right":180,
-            "ankle_left":90,
-            "ankle_right":180
+            "armpit_left": 90,
+            "armpit_right": 90,
+            "elbow_left": 180,
+            "elbow_right": 180,
+            "hip_left": 90,
+            "hip_right": 180,
+            "knee_left": 180,
+            "knee_right": 180,
+            "ankle_left": 90,
+            "ankle_right": 180,
         },
         "tree_pose": {
-            "armpit_left":180,
-            "armpit_right":180,
-            "elbow_left":120,
-            "elbow_right":120,
-            "hip_left":180,
-            "hip_right":120,
-            "knee_left":180,
-            "knee_right":30,
-            "ankle_left":180,
-            "ankle_right":90
-        } 
+            "armpit_left": 180,
+            "armpit_right": 180,
+            "elbow_left": 120,
+            "elbow_right": 120,
+            "hip_left": 180,
+            "hip_right": 120,
+            "knee_left": 180,
+            "knee_right": 30,
+            "ankle_left": 180,
+            "ankle_right": 90,
+        },
     }
+    """
+    Dictionary defining ground truth angle values for various yoga poses.
+    These values are used for pose correction and feedback.
+    """
 
     LANDMARKS_LIST = {
-    "left_shoulder": [], "right_shoulder": [],
-    "left_elbow": [], "right_elbow": [],
-    "left_wrist": [], "right_wrist": [],
-    "left_hip": [], "right_hip": [],
-    "left_knee": [], "right_knee": [],
-    "left_ankle": [], "right_ankle": [],
-    "left_heel": [], "right_heel": [],
-    "left_foot_index": [], "right_foot_index": [],
+        "left_shoulder": [],
+        "right_shoulder": [],
+        "left_elbow": [],
+        "right_elbow": [],
+        "left_wrist": [],
+        "right_wrist": [],
+        "left_hip": [],
+        "right_hip": [],
+        "left_knee": [],
+        "right_knee": [],
+        "left_ankle": [],
+        "right_ankle": [],
+        "left_heel": [],
+        "right_heel": [],
+        "left_foot_index": [],
+        "right_foot_index": [],
     }
+    """
+    Dictionary to store landmark coordinates during pose estimation.
+    Initialized with empty lists for each landmark.
+    """
 
-    PERSONALIZED_MESSAGES={
+    PERSONALIZED_MESSAGES = {
         "chair_pose": {
-            "armpit_left":"Raise your arms overhead!",
-            "armpit_right":"Raise your arms overhead!",
-            "elbow_left":"Keep your elbows straight!",
-            "elbow_right":"Keep your elbows straight!",
-            "hip_left":"Shift your weight back into your heels!",
-            "hip_right":"Shift your weight back into your heels!",
-            "knee_left":"Bend your knees to make your thighs are parallel to the ground!",
-            "knee_right":"Bend your knees to make your thighs are parallel to the ground!",
+            "armpit_left": "Raise your arms overhead!",
+            "armpit_right": "Raise your arms overhead!",
+            "elbow_left": "Keep your elbows straight!",
+            "elbow_right": "Keep your elbows straight!",
+            "hip_left": "Shift your weight back into your heels!",
+            "hip_right": "Shift your weight back into your heels!",
+            "knee_left": "Bend your knees to make your thighs are parallel to the ground!",
+            "knee_right": "Bend your knees to make your thighs are parallel to the ground!",
         },
         "downward_facing_dog": {
-            "armpit_left":"Stretch your arms!",
-            "armpit_right":"Stretch your arms!",
-            "elbow_left":"Stretch your arms!",
-            "elbow_right":"Stretch your arms!",
-            "hip_left":"Lengthen your spine!",
-            "hip_right":"Lengthen your spine!",
-            "knee_left":"Keep your legs straight!",
-            "knee_right":"Keep your legs straight!",
-            "ankle_left":"Take care not to strain your ankles!",
-            "ankle_right":"Take care not to strain your ankles!"
+            "armpit_left": "Stretch your arms!",
+            "armpit_right": "Stretch your arms!",
+            "elbow_left": "Stretch your arms!",
+            "elbow_right": "Stretch your arms!",
+            "hip_left": "Lengthen your spine!",
+            "hip_right": "Lengthen your spine!",
+            "knee_left": "Keep your legs straight!",
+            "knee_right": "Keep your legs straight!",
+            "ankle_left": "Take care not to strain your ankles!",
+            "ankle_right": "Take care not to strain your ankles!",
         },
         "revolved_triangle": {
-            "armpit_left":"Straighten your arms to make them in one line!",
-            "armpit_right":"Straighten your arms to make them in one line!",
-            "elbow_left":"Don't bend your elbows!",
-            "elbow_right":"Don't bend your elbows!",
-            "knee_left":"Keep your legs straight!",
-            "knee_right":"Keep your legs straight!",
-            "ankle_left":"Press your ankle firmly into the ground!",
-            "ankle_right":"Press your ankle firmly into the ground!"
+            "armpit_left": "Straighten your arms to make them in one line!",
+            "armpit_right": "Straighten your arms to make them in one line!",
+            "elbow_left": "Don't bend your elbows!",
+            "elbow_right": "Don't bend your elbows!",
+            "knee_left": "Keep your legs straight!",
+            "knee_right": "Keep your legs straight!",
+            "ankle_left": "Press your ankle firmly into the ground!",
+            "ankle_right": "Press your ankle firmly into the ground!",
         },
         "half_moon": {
-            "armpit_left":"Straighten your arms to make them in one line!",
-            "armpit_right":"Straighten your arms to make them in one line!",
-            "elbow_left":"Don't bend your elbows!",
-            "elbow_right":"Don't bend your elbows!",
-            "hip_left":"Press your leg firmly into the ground!",
-            "hip_right":"Lift your leg higher!",
-            "knee_left":"Keep your legs straight!",
-            "knee_right":"Keep your legs straight!"
+            "armpit_left": "Straighten your arms to make them in one line!",
+            "armpit_right": "Straighten your arms to make them in one line!",
+            "elbow_left": "Don't bend your elbows!",
+            "elbow_right": "Don't bend your elbows!",
+            "hip_left": "Press your leg firmly into the ground!",
+            "hip_right": "Lift your leg higher!",
+            "knee_left": "Keep your legs straight!",
+            "knee_right": "Keep your legs straight!",
         },
         "tree_pose": {
-            "armpit_left":"Stretch your arms towards the ceiling!",
-            "armpit_right":"Stretch your arms towards the ceiling!",
-            "elbow_left":"Don't bend your elbows too much",
-            "elbow_right":"Don't bend your elbows too much",
-            "hip_left":"Make sure your leg on the ground is straight!",
-            "hip_right":"Lift your leg higher towards you!",
-            "knee_left":"Make sure your leg on the ground is straight!",
-            "knee_right":"Lift your leg higher towards you!"
-        } 
+            "armpit_left": "Stretch your arms towards the ceiling!",
+            "armpit_right": "Stretch your arms towards the ceiling!",
+            "elbow_left": "Don't bend your elbows too much",
+            "elbow_right": "Don't bend your elbows too much",
+            "hip_left": "Make sure your leg on the ground is straight!",
+            "hip_right": "Lift your leg higher towards you!",
+            "knee_left": "Make sure your leg on the ground is straight!",
+            "knee_right": "Lift your leg higher towards you!",
+        },
     }
-
-
-
+    """
+    Dictionary containing personalized feedback messages for each yoga pose
+    and specific body parts.  These messages are displayed to the user
+    to help them correct their pose.
+    """
--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -2,18 +2,27 @@
 EECS 6692: Deep Learning on the Edge
 File contains functions to display images and annotations
 """
+
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 
-def display_image(image_path):
-    # Read the image from the specified path
+
+def display_image(image_path: str) -> None:
+    """
+    Display an image from the specified path.
+
+    Args:
+        image_path (str): Path to the image file.
+
+    Returns:
+        None
+    """
     print(image_path)
     image = cv2.imread(image_path)
 
-    # Check if the image was successfully loaded
     if image is None:
         print("Error: Unable to read the image.")
         return
@@ -26,14 +35,20 @@ def display_image(image_path):
     plt.axis('off')  # Hide axis
     plt.show()
 
-def display_annotated_image(image):
-    # Check if the image was successfully loaded
+
+def display_annotated_image(image: np.ndarray) -> None:
+    """
+    Display an annotated image.
+
+    Args:
+        image (np.ndarray): The image to display.
+
+    Returns:
+        None
+    """
     if image is None:
         print("Error: Unable to read the image.")
         return
-
-    # Convert the image from BGR to RGB (OpenCV uses BGR by default)
-    # image_rgb = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     # Display the image using Matplotlib
     plt.imshow(image)
@@ -41,25 +56,38 @@ def display_annotated_image(image):
     plt.show()
 
 
-# source: 
-# https://colab.research.google.com/github/googlesamples/mediapipe/blob/main/examples/pose_landmarker/python/%5BMediaPipe_Python_Tasks%5D_Pose_Landmarker.ipynb#scrollTo=h2q27gKz1H20
+def draw_landmarks_on_image(rgb_image: np.ndarray, detection_result) -> np.ndarray:
+    """
+    Draw pose landmarks on the input RGB image.
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-  pose_landmarks_list = detection_result.pose_landmarks
-  annotated_image = np.copy(rgb_image)
+    Args:
+        rgb_image (np.ndarray): The input RGB image.
+        detection_result: The pose detection result containing landmarks.
 
-  # Loop through the detected poses to visualize.
-  for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
+    Returns:
+        np.ndarray: The annotated image with pose landmarks drawn.
 
-    # Draw the pose landmarks.
-    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    pose_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
-  return annotated_image
+    Source:
+    https://colab.research.google.com/github/googlesamples/mediapipe/blob/main/examples/pose_landmarker/python/%5BMediaPipe_Python_Tasks%5D_Pose_Landmarker.ipynb#scrollTo=h2q27gKz1H20
+    """
+    pose_landmarks_list = detection_result.pose_landmarks
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected poses to visualize.
+    for pose_landmarks in pose_landmarks_list:
+        # Convert pose landmarks to proto format
+        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        pose_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z)
+            for landmark in pose_landmarks
+        ])
+
+        # Draw the pose landmarks on the image
+        solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            pose_landmarks_proto,
+            solutions.pose.POSE_CONNECTIONS,
+            solutions.drawing_styles.get_default_pose_landmarks_style()
+        )
+
+    return annotated_image
